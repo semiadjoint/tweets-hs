1096801f6bd136bf88c1f7f20f7af2ffc0cd1339
Andrew Mohrland <noreply@mohr.land>
2018-01-18 15:51:23 -0800

    Pretty script colors

--- watch.bash
+++ watch.bash
@@ -1,4 +1,7 @@
 #! /usr/bin/env bash
 
 exec ag -g hs$ | \
-  entr -s "echo && echo && cabal build && echo Built at $(date --iso-8601=seconds)"
+  entr -s 'echo && \
+          echo && \
+          cabal build && \
+          printf "\e[32mBuilt at $(date --iso-8601=seconds)\n\e[0m"'
