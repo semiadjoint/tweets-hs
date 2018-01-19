775bd923893ca385e9b34902523e7fe00735aace
Andrew Mohrland <noreply@mohr.land>
2018-01-19 11:02:48 -0800

    More color for build script

--- watch.bash
+++ watch.bash
@@ -4,4 +4,5 @@ exec ag -g hs$ | \
   entr -s 'echo && \
           echo && \
           cabal build && \
-          printf "\e[32mBuilt at $(date --iso-8601=seconds)\n\e[0m"'
+          printf "\e[32mBuilt at $(date --iso-8601=seconds)\n\e[0m" || \
+          printf "\e[31mBuild failed at $(date --iso-8601=seconds)\n\e[0m"'
